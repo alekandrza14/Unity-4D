7e720333e4860d71789c9915865bbd41f80e6a69
unauticna <66287905+alekandrza14@users.noreply.github.com>
2024-06-10 04:24:37 +0500

    0.1

---
+++ Assets/Scripts/MultyTransform.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteAlways]
+public class MultyTransform : MonoBehaviour
+{
+    public float W_Position,H_Position;
+    private void Update()
+    {
+        MultyObject[] g = FindObjectsByType<MultyObject>(FindObjectsSortMode.InstanceID);
+        foreach (MultyObject obj in g)
+        {
+            obj.Update();
+        }
+    }
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(0,0,600,60),"World Position x : " + transform.position.x + " y : " + transform.position.y + " z : " + transform.position.z + " w : " + W_Position + " h : " + H_Position);
+    }
+}
